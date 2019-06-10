73ca58c5d45a16b3a045ec2409e8e9731d19795b
Zain Ul Hassan <zain.hassan1212@gmail.com>
2019-06-10 12:30:25 +0500

    update sidedrawer

--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -7,51 +7,134 @@ import {
   Platform,
   Image
 } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
-import Logo from "../../assets/logo.png";
-import Hr from "../../components/UI/Hr/Hr";
-import HeadingText from "../../components/UI/HeadingText/HeadingText";
+import { CheckBox, Divider } from "react-native-elements";
+import Button from "../../components/UI/Button/Button";
 
 class SideDrawer extends React.Component {
+  state = {
+    checkboxes: [
+      {
+        id: 1,
+        title: "Desktop",
+        checked: false
+      },
+      {
+        id: 2,
+        title: "Mobile",
+        checked: false
+      },
+      {
+        id: 3,
+        title: "Web",
+        checked: false
+      },
+      {
+        id: 4,
+        title: "C#",
+        checked: false
+      },
+      ,
+      {
+        id: 5,
+        title: "C++",
+        checked: false
+      },
+
+      {
+        id: 6,
+        title: "Java",
+        checked: false
+      },
+
+      {
+        id: 7,
+        title: "Paid",
+        checked: false
+      },
+
+      {
+        id: 8,
+        title: "Free",
+        checked: false
+      }
+    ]
+  };
+
+  toggleCheckbox = id => {
+    const changedCheckbox = this.state.checkboxes.find(cb => cb.id === id);
+
+    changedCheckbox.checked = !changedCheckbox.checked;
+
+    const checkboxes = Object.assign(this.state.checkboxes, changedCheckbox);
+
+    this.setState({ checkboxes });
+    console.log(checkboxes);
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity>
-          <HeadingText>Filters</HeadingText>
-          {/* <View style={styles.logoContainer}>
-            <Image source={Logo} style={styles.logo} />
+        <View style={styles.subContainer}>
+          <Text style={styles.heading}>Platform</Text>
+
+          {this.state.checkboxes.map((cb, i) => {
+            console.log(i);
+            // if (i < 3) {
+            //   return (
+            //     <CheckBox
+            //       textStyle={styles.textCheckbox}
+            //       containerStyle={styles.checkbox}
+            //       key={cb.id}
+            //       title={cb.title}
+            //       checked={cb.checked}
+            //       onPress={() => this.toggleCheckbox(cb.id)}
+            //     />
+            //   );
+            // }
+          })}
+
+          <Divider style={styles.divider} />
+          <Text style={styles.heading}>Technologies</Text>
+          {this.state.checkboxes.map((cb, i) => {
+            console.log(i);
+            if (i < 3) {
+              return (
+                <CheckBox
+                  textStyle={styles.textCheckbox}
+                  containerStyle={styles.checkbox}
+                  key={cb.id}
+                  title={cb.title}
+                  checked={cb.checked}
+                  onPress={() => this.toggleCheckbox(cb.id)}
+                />
+              );
+            }
+          })}
+
+          <Divider style={styles.divider} />
+          <Text style={styles.heading}>Price</Text>
+
+          {this.state.checkboxes.map((cb, i) => {
+            if (i > 6) {
+              return (
+                <CheckBox
+                  textStyle={styles.textCheckbox}
+                  containerStyle={styles.checkbox}
+                  key={cb.id}
+                  title={cb.title}
+                  checked={cb.checked}
+                  onPress={() => this.toggleCheckbox(cb.id)}
+                />
+              );
+            }
+          })}
+
+          <View style={styles.buttons}>
+            <TouchableOpacity>
+              <Button title="Apply" color="#05C0BA" />
+            </TouchableOpacity>
           </View>
-          <Hr />
-          <View style={styles.drawerItem}>
-            <Icon
-              name={
-                Platform.OS === "android" ? "md-help-circle" : "ios-help-circle"
-              }
-              size={30}
-              color="#aaa"
-              style={styles.drawerItemIcon}
-            />
-            <Text>Support</Text>
-          </View>
-          <View style={styles.drawerItem}>
-            <Icon
-              name={Platform.OS === "android" ? "md-share" : "ios-share"}
-              size={30}
-              color="#aaa"
-              style={styles.drawerItemIcon}
-            />
-            <Text>Share our Softforest app</Text>
-          </View>
-          <View style={styles.drawerItem}>
-            <Icon
-              name={Platform.OS === "android" ? "md-log-out" : "ios-log-out"}
-              size={30}
-              color="#aaa"
-              style={styles.drawerItemIcon}
-            />
-            <Text>Sign Out</Text>
-          </View> */}
-        </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -63,23 +146,27 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     flex: 1
   },
-  drawerItem: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 10
+  subContainer: {
+    marginLeft: 15
   },
-  drawerItemIcon: {
-    marginRight: 10
+  heading: {
+    fontWeight: "bold",
+    fontSize: 16
   },
-  logoContainer: {
-    width: 150,
-    height: 100,
-    padding: 10
+  checkbox: {
+    margin: 0,
+    backgroundColor: "transparent",
+    borderWidth: 0,
+    marginLeft: -10
   },
-  logo: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain"
+  textCheckbox: {
+    fontWeight: "normal"
+  },
+  divider: {
+    margin: 10
+  },
+  buttons: {
+    padding: 20
   }
 });
 
